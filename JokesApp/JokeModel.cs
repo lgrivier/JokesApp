8149a592c03e371797dd6ca7afad95346a6fe104
Lauren <Lgrivier@hawkmail.hfcc.edu>
2020-08-17 22:49:37 -0400

    added API classes

---
+++ JokesApp/JokeModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JokesApp
+{
+    public class JokeModel
+    {
+    //    public string text { set; get; }
+
+        public string copyright { set; get; }
+
+        
+    } 
+}
---
+++ JokesApp/JokeProcessor.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JokesApp
+{
+    class JokeProcessor
+    {
+        public static async Task<JokeModel> LoadJoke()
+        {
+            string url = "https://api.jokes.one/jod?category=animal";
+
+            using (System.Net.Http.HttpResponseMessage response = await ApiHelper.ApiClient.GetAsync(url))
+            {
+                if (response.IsSuccessStatusCode)
+                {
+                    JokeContentModel contents = await response.Content.ReadAsAsync<JokeContentModel>();
+
+                    return contents.contents;
+                }
+                else
+                {
+                    throw new Exception(response.ReasonPhrase);
+                }
+            }
+        }
+    }
+}
